de60a6b47d1e4b23e86112538fd08b382800f2f0
Herman Lee <herman@fb.com>
2020-01-21 18:13:20 -0800

    Prevent super from modifying read-only instances if sql_log_bin is enabled
    Summary:
    For read-only slaves replicas, it is possible for super to generate
    transactions that are logged to the binlog, resulting in a different
    binlog stream for these replicas than for the master. Add a fence to the
    server to prevent these types of transactions from being executing by
    accident.
    
    Reviewed By: lth
    
    Differential Revision: D19510758
    
    fbshipit-source-id: 1fd15dc75bf

--- sql/auth/sql_authorization.cc
+++ sql/auth/sql_authorization.cc
@@ -1893,8 +1893,13 @@ bool check_readonly(THD *thd, bool err_if_readonly) {
 
   /* super_read_only=OFF and user has SUPER privilege,
   do not prohibit operation:
+  However, if sql_log_bin is enabled for super, prevent the write to avoid
+  local binlog transactions.
   */
-  if (is_super && !opt_super_readonly) return false;
+  if (is_super && !opt_super_readonly &&
+      (enable_super_log_bin_read_only ||
+       !(thd->variables.option_bits & OPTION_BIN_LOG)))
+    return false;
 
   /* throw error in standardized way if requested: */
   if (err_if_readonly) err_readonly(thd);
@@ -1917,7 +1922,8 @@ void err_readonly(THD *thd) {
                    thd->security_context()
                        ->has_global_grant(STRING_WITH_LEN("CONNECTION_ADMIN"))
                        .first
-               ? "--super-read-only"
+               ? (opt_super_readonly ? "--super-read-only"
+                                     : "--sql_log_bin and --read-only")
                : "--read-only",
            extra_info.c_str());
 }
