facebook/mysql-5.6

bring in Raft_repl_observer
Summary:
Almost a direct port of the feature in 5.6 except for minor visual changes and
some minor changes related to 8.0 thread handling

Also brings in the basic raft_listener_queue without the implemented functions

Reviewed By: anirbanr-fb

Differential Revision: D21670710

fbshipit-source-id: 9e571cdc203

sql/sql_class.h
@@ -1618,6 +1618,14 @@ class THD : public MDL_context_owner,
   // but currently its design doesn't allow that.
   NET net;        // client connection descriptor
   String packet;  // dynamic buffer for network I/O
+
+  /* The term and index that need to be communicated across different raft
+   * plugin hooks. These fields are not protected by locks since they are
+   * accessed by the same THD serially during different stages of ordered commit
+   * today. Protect this by locks if things change in future. */
+  int64_t term_= -1;
+  int64_t index_= -1;
+
  public:
   bool has_net_vio() const noexcept { return net.vio != nullptr; }
   const Vio *get_net_vio() const noexcept { return net.vio; }
@@ -2367,6 +2375,12 @@ class THD : public MDL_context_owner,
   }
 
   /**@}*/
+
+  /* Get the trans marker i.e (term, index) tuple stashed in this THD */
+  void get_trans_marker(int64_t *term, int64_t *index) const;
+
+  /* Stash the trans marker i.e (term, index) tuple in this THD */
+  void set_trans_marker(int64_t term, int64_t index);
 
   /*
     Error code from committing or rolling back the transaction.

voodam/percona-server

sql/sql_class.h
@@ -1662,6 +1662,14 @@ class THD : public MDL_context_owner,
   // but currently its design doesn't allow that.
   NET net;        // client connection descriptor
   String packet;  // dynamic buffer for network I/O
+
+  /* The term and index that need to be communicated across different raft
+   * plugin hooks. These fields are not protected by locks since they are
+   * accessed by the same THD serially during different stages of ordered commit
+   * today. Protect this by locks if things change in future. */
+  int64_t term_ = -1;
+  int64_t index_ = -1;
+
  public:
   bool has_net_vio() const noexcept { return net.vio != nullptr; }
   const Vio *get_net_vio() const noexcept { return net.vio; }
@@ -2417,6 +2425,12 @@ class THD : public MDL_context_owner,
   }
 
   /**@}*/
+
+  /* Get the trans marker i.e (term, index) tuple stashed in this THD */
+  void get_trans_marker(int64_t *term, int64_t *index) const;
+
+  /* Stash the trans marker i.e (term, index) tuple in this THD */
+  void set_trans_marker(int64_t term, int64_t index);
 
   /*
     Error code from committing or rolling back the transaction.