facebook/mysql-5.6

Port v5.1 Extra Stats: Hide BinLog Threads
Summary:
Moves binlog threads from Threads_running to Threads_binlog_client.

The original patch has been moderately reworked to match current MySQL 8.0 codebase.

(Original Reviewed By: chip, rudradevbasak)

Reference patch: https://github.com/facebook/mysql-5.6/commit/5ed1244

Reviewed By: asandryh

Differential Revision: D6478040

fbshipit-source-id: 771d581ced5

sql/rpl_master.cc
@@ -912,6 +912,7 @@ bool com_binlog_dump(THD *thd, char *packet, size_t packet_length) {
   ushort flags = 0;
   const uchar *packet_position = (uchar *)packet;
   size_t packet_bytes_todo = packet_length;
+  Global_THD_manager *thd_manager = Global_THD_manager::get_instance();
 
   thd->status_var.com_other++;
   thd->enable_slow_log = opt_log_slow_admin_statements;
@@ -933,8 +934,15 @@ bool com_binlog_dump(THD *thd, char *packet, size_t packet_length) {
 
   query_logger.general_log_print(thd, thd->get_command(), "Log: '%s'  Pos: %ld",
                                  packet + 10, (long)pos);
+
+  thd_manager->inc_thread_binlog_client();
+  thd_manager->dec_thread_running();
+
   mysql_binlog_send(thd, thd->mem_strdup(packet + 10), (my_off_t)pos, NULL,
                     flags);
+
+  thd_manager->inc_thread_running();
+  thd_manager->dec_thread_binlog_client();
 
   unregister_slave(thd, true, true /*need_lock_slave_list=true*/);
   /*  fake COM_QUIT -- if we get here, the thread needs to terminate */

voodam/percona-server

fbshipit-source-id: defd48fe263

sql/rpl_master.cc
@@ -935,6 +935,7 @@ bool com_binlog_dump(THD *thd, char *packet, size_t packet_length) {
 
   query_logger.general_log_print(thd, thd->get_command(), "Log: '%s'  Pos: %ld",
                                  packet + 10, (long)pos);
+
   mysql_binlog_send(thd, thd->mem_strdup(packet + 10), (my_off_t)pos, nullptr,
                     flags);
 
@@ -1006,9 +1007,17 @@ error_malformed_packet:
 
 void mysql_binlog_send(THD *thd, char *log_ident, my_off_t pos,
                        Gtid_set *slave_gtid_executed, uint32 flags) {
+  auto *thd_manager = Global_THD_manager::get_instance();
+
+  thd_manager->inc_thread_binlog_client();
+  thd_manager->dec_thread_running();
+
   Binlog_sender sender(thd, log_ident, pos, slave_gtid_executed, flags);
 
   sender.run();
+
+  thd_manager->inc_thread_running();
+  thd_manager->dec_thread_binlog_client();
 }
 
 /**