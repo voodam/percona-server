facebook/mysql-5.6

Adding SUM_ROWS_DELETED,SUM_ROWS_INSERTED and SUM_ROWS_UPDATED columns to events_statements_summary_by_user_by_event_name table.
Summary:
Extended events_statements_summary_by_user_by_event_name table to track deleted rows under the column of SUMS_ROWS_DELETED.
Also added columns to track inserted and deleted rows.

Reviewed By: lth

Differential Revision: D16444830

fbshipit-source-id: f34b10284a6

include/pfs_statement_provider.h
@@ -72,6 +72,15 @@ void pfs_set_statement_rows_sent_v1(PSI_statement_locker *locker,
 void pfs_set_statement_rows_examined_v1(PSI_statement_locker *locker,
                                         ulonglong count);
 
+void pfs_inc_statement_rows_deleted_v1(PSI_statement_locker *locker,
+                                        ulonglong count);
+
+void pfs_inc_statement_rows_inserted_v1(PSI_statement_locker *locker,
+                                        ulonglong count);
+
+void pfs_inc_statement_rows_updated_v1(PSI_statement_locker *locker,
+                                        ulonglong count);
+
 void pfs_inc_statement_created_tmp_disk_tables_v1(PSI_statement_locker *locker,
                                                   ulong count);
 

voodam/percona-server

fbshipit-source-id: 50ed5285c90

include/pfs_statement_provider.h
@@ -76,6 +76,15 @@ void pfs_set_statement_rows_sent_v2(PSI_statement_locker *locker,
 void pfs_set_statement_rows_examined_v2(PSI_statement_locker *locker,
                                         ulonglong count);
 
+void pfs_inc_statement_rows_deleted_v2(PSI_statement_locker *locker,
+                                       ulonglong count);
+
+void pfs_inc_statement_rows_inserted_v2(PSI_statement_locker *locker,
+                                        ulonglong count);
+
+void pfs_inc_statement_rows_updated_v2(PSI_statement_locker *locker,
+                                       ulonglong count);
+
 void pfs_inc_statement_created_tmp_disk_tables_v2(PSI_statement_locker *locker,
                                                   ulong count);
 