facebook/mysql-5.6

ordered_commit changes to talk to raft_replication hooks
Summary:
Ports the ordered_commit flow to raft. This enables data replication through
raft.

Some changes compared to 5.6:
1. Writing to binlog is different in 5.6 and 8.0. IO_CACHE for trxs are not
exposed directly, but is managed by certain stream objects. This diff uses the
getters added in stacked diffs to get the IO_CACHE which is then passed to the
plugin for transmittal.
2. There is no gtid and metadata event in the IO_CACHE (as happens in 5.6).
These events are written directly to binlog file during commit time. When raft
is enabled, gtid event and metadata event are written to flat memory buffers.
These buffers are passed to `before_flush` call of raft plugin. The plugin will
take care of patching and writing these events to binlog file (to be
implemented later).
3. There is no semi-sync stage in ordered_commit. Semisync plugin uses
binlog_storage observer's `after_sync` hook. Hence, in raft mode we use the
commit-stage queue to block for consensus commit. A new method
`process_consensus_queue()` is added which takes care of doing this (similar in
spirit to 5.6's `process_semisync_stage_queue()` with minor difference
4. gtid_executed and gtid_state maintainance is different in 5.6 and 8.0. 8.0
also adds a gtid_executed table which needs to be maintained correctly when trxs
are explicitly rolled back due to consensus errors

Reviewed By: anirbanr-fb

Differential Revision: D21795096

fbshipit-source-id: de26dd13abb

sql/replication.h
@@ -25,6 +25,7 @@
 
 #include "my_thread_local.h"         // my_thread_id
 #include "mysql/psi/mysql_thread.h"  // mysql_mutex_t
+#include "raft_listener_queue_if.h" // RaftReplicateMsgOpType
 #include "rpl_context.h"
 #include "sql/handler.h"  // enum_tx_isolation
 
@@ -722,6 +723,8 @@ typedef struct Raft_replication_param {
   const char* host_or_ip = nullptr;
   int64_t term = -1;
   int64_t index = -1;
+  uchar* gtid_buffer = nullptr;
+  uchar* metadata_buffer = nullptr;
 } Raft_replication_param;
 
 /**
@@ -749,13 +752,13 @@ typedef struct Raft_replication_observer {
 
      @param param Observer common parameter
      @param cache IO_CACHE containing binlog events for the txn
-     @param noop  Is this a Raft NOOP event being faked as a Rotate Event
+     @param op_type The type of operation for which before_flush is called
 
      @retval 0 Sucess
      @retval 1 Failure
   */
   int (*before_flush)(Raft_replication_param *param, IO_CACHE* cache,
-                      bool no_op);
+                      RaftReplicateMsgOpType op_type);
 
   /**
      This callback is called once upfront to setup the appropriate

voodam/percona-server

sql/replication.h
@@ -25,6 +25,7 @@
 
 #include "my_thread_local.h"         // my_thread_id
 #include "mysql/psi/mysql_thread.h"  // mysql_mutex_t
+#include "raft_listener_queue_if.h"  // RaftReplicateMsgOpType
 #include "rpl_context.h"
 #include "sql/handler.h"  // enum_tx_isolation
 
@@ -736,6 +737,8 @@ typedef struct Raft_replication_param {
   const char *host_or_ip = nullptr;
   int64_t term = -1;
   int64_t index = -1;
+  uchar *gtid_buffer = nullptr;
+  uchar *metadata_buffer = nullptr;
 } Raft_replication_param;
 
 /**
@@ -763,13 +766,13 @@ typedef struct Raft_replication_observer {
 
      @param param Observer common parameter
      @param cache IO_CACHE containing binlog events for the txn
-     @param noop  Is this a Raft NOOP event being faked as a Rotate Event
+     @param op_type The type of operation for which before_flush is called
 
      @retval 0 Sucess
      @retval 1 Failure
   */
   int (*before_flush)(Raft_replication_param *param, IO_CACHE *cache,
-                      bool no_op);
+                      RaftReplicateMsgOpType op_type);
 
   /**
      This callback is called once upfront to setup the appropriate